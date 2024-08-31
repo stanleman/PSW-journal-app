c6cb80393bee03e001f6f9da324cde549d4d969d
ThatFit3 <m.nurfitri08@gmail.com>
2024-08-31 22:17:59 +0800

    added .env and not exposing our firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:psw_journal_app/firebase_options.dart';
 import 'package:psw_journal_app/pages/login/login.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
